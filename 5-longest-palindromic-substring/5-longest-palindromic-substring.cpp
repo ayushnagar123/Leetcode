e5e4b1c49d4cbccd84d42ea8241b9d12fbe26e18
Ayush Nagar <ayushnagar123.atwork@gmail.com>
2022-02-22 11:25:15 +0530

    Time: 1864 ms (5.01%), Space: 465.6 MB (6.43%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    vector<vector<bool>> dp;
-    
-    string ans;
-    
     bool isPalindrome(string s){
         int i=0, j = s.size() - 1;
         while(i<j){
@@ -16,27 +12,17 @@ public:
         return true;
     }
     
-    int longestCommonSubstring(string x, string y, int n, int m){
-        vector<int> row(y.size()+1, -1);
-        vector<vector<int>> dp(x.size()+1,row);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                }
-            }
-        }
+    vector<vector<int>> dp;
+    string ans="";
+    void lcs(string x, string y){
         int max = 0;
-        int end_index = 0;
+        int n = x.size();
         for(int i=1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
-                if(x[i-1] == y[j - 1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    if(max < dp[i][j]){
-                        max = dp[i][j];
-                    }
-                    if(ans.size() < dp[i][j] and isPalindrome(x.substr(i-dp[i][j], dp[i][j]))) {
-                        ans = x.substr(i-dp[i][j], dp[i][j]);
+            for(int j = 1;j<=n;j++){
+                if(x[i-1]==y[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    if(ans.size() < dp[i][j] and isPalindrome(x.substr(i - dp[i][j], dp[i][j]))) {
+                        ans = x.substr(i - dp[i][j], dp[i][j]);
                     }
                 }
                 else {
@@ -44,14 +30,14 @@ public:
                 }
             }
         }
-        return max;
     }
     
-    
     string longestPalindrome(string s) {
-        string p = s;
-        reverse(p.begin(), p.end());
-        cout<<longestCommonSubstring(s, p ,s.size(), p.size());
+       string y = s;
+        reverse(y.begin(), y.end());
+        vector<int> row(s.size()+1, 0);
+        dp = vector<vector<int>> (s.size()+1, row);
+        lcs(s, y);
         return ans;
     }
 };
