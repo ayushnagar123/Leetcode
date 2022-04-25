b47beb61a4e44feef3207b877787558fabe50b18
Ayush Nagar <ayushnagar123.atwork@gmail.com>
2022-04-26 03:15:46 +0530

    Time: 32 ms (41.64%), Space: 7.5 MB (93.41%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,52 +1,54 @@
 class Solution {
 public:
-    string minWindow(string s, string p) {
-        int i = 0, j = 0, mini= -1, minj = -1;
-        int n = s.size();
-        int min = n+1;
+    string minWindow(string s, string t) {
         map<char, int> m;
-        for(auto ch: p){
-            m[ch]++;
+        for(char c: t){
+            m[c]++;
         }
         int count = m.size();
-        int window_size;
+        int n = s.size();
+        
+        int i = 0, j = 0;
+        int min = n+1;
+        int ws;
+        int s_ans = min;
+        
         while(j<n){
-            window_size = j - i + 1;
-            if(m.find(s[j]) != m.end()){
-                m[s[j]]--;
-                if(m[s[j]]==0){
-                    count--;
-                }
-            }
-            if(count == 0){
-                if(min > window_size) {
-                    min = window_size;
-                    mini = i;
-                    minj = j;
-                }
-                while(count==0 && i <= j){
-                    if(m.find(s[i]) != m.end()){
+            char c = s[j];
+            ws = j-i+1;
+            
+            if(m.find(c)!=m.end()){
+                m[c]--;
+                if(m[c]==0){count--;}
+                if(count==0 and min > ws){min = ws;s_ans = i;}
+                while(count==0 and i<=j){
+                    if(m.find(s[i])!=m.end()){
                         m[s[i]]++;
                         if(m[s[i]]==1){
                             count++;
+                            i++;
+                            break;
                         }
                     }
-                    if(min > (j - i + 1)) {
-                        min = j - i +1;
-                        mini = i;
-                        minj = j;
-                    }
                     i++;
+                    if(count==0){
+                        ws = j - i + 1;
+                        if(min > ws){
+                            min = ws;
+                            s_ans = i;
+                        }
+                    }
                 }
             }
             j++;
         }
-        string ans="";
-        if(mini !=-1){
-            for(int k = mini;k<=minj;k++){
-                ans+=s[k];
-            }
+        string a = "";
+        if(s_ans == (n+1)){
+            return a;
         }
-        return ans;
+        for(int i = s_ans;i<s_ans+min;i++){
+            a+=s[i];
+        }
+        return a;
     }
 };
